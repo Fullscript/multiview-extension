d08a2bcebbfdbc4a3a34203a41b432ae6b147abf
Andrew Scullion <andrew.scullion@fullscript.com>
2023-05-05 06:51:38 -0400

    Display contents of root directory

--- src/multi_view_provider.ts
+++ src/multi_view_provider.ts
@@ -15,13 +15,32 @@ export class MultiViewProvider implements vscode.TreeDataProvider<ViewItem> {
             return Promise.resolve([])
         }
         vscode.window.showInformationMessage('You haz a workspace!')
-        return Promise.resolve([])
+        let stuff = fs.readdirSync(this.workspaceRoot)
+        
+        return Promise.resolve(stuff.map((fileName) => {
+            let isDirectory = fs.statSync(path.join(this.workspaceRoot, fileName)).isDirectory()
+            let state = isDirectory ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None
+            return {fileName, state}
+            // console.log({fileName, state})
+            // return new ViewItem(fileName, state)
+        }).sort((a, b) => {
+            let diff = b.state - a.state
+            if (diff != 0) {
+                return diff
+            } else {
+                return a.fileName < b.fileName ? -1 : 1
+            }
+        }).map((result) => {
+            return new ViewItem(result.fileName, result.state)
+        })
+        )
     }
-
 
 }
 
 
 class ViewItem extends vscode.TreeItem {
-
+    constructor(label: string | vscode.TreeItemLabel, collapsibleState?: vscode.TreeItemCollapsibleState) {
+        super(label, collapsibleState)
+    }
 }
